ea5d4a292b4c18bdf821b418070b9baecbb46fb4
codehub1224 <codehub.apps@gmail.com>
2025-06-22 01:58:04 +0530

    Fix Font Awesome CSS Import

--- nuxt.config.js
+++ nuxt.config.js
@@ -101,8 +101,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/fonts.css',
-    '@fortawesome/fontawesome-svg-core/styles.css'
+    '~/assets/fonts.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
