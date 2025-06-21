dc8ce62a4a3b32d721aef15632dd14138bb110b1
codehub1224 <codehub.apps@gmail.com>
2025-06-22 02:12:06 +0530

    Fix Watchpack ENOTDIR error in nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -112,7 +112,6 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: {
     dirs: [
-      '~/components',
       {
         path: '~/components',
         ignore: ['**/GroundHandlingDescription.vue']
