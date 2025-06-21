24fd042e45f89b9ba5f36d7e7768db87d4bf9848
codehub1224 <codehub.apps@gmail.com>
2025-06-22 01:48:11 +0530

    Updated fontawesome.js

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCloudUploadAlt } from '@fortawesome/free-solid-svg-icons'
+
 import { 
   faPhoneAlt, 
   faCloudArrowUp,
